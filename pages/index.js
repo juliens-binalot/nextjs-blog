433219ab191833e2046bf08502d5f97dca5c1058
Julien Macandili <jqmac@Juliens-Air.modem>
2023-06-02 14:05:30 +1000

    updated custom nextjs blog

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -11,8 +23,23 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read <Link href="/posts/first-post"> this page! </Link>
         </h1>
+
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
 
         <p className={styles.description}>
           Get started by editing <code>pages/index.js</code>
